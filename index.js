5ef44bd44e721862722e8cc7d94619134f7a118d
lauraszm <lauraszm@gmail.com>
2024-08-26 22:48:09 -0300

    feat: cambio de color

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let boton = document.getElementById('boton');
+const boton = document.getElementById('boton');
 boton.addEventListener('click', function modificarH1(){
     let extra = document.getElementById('extra');
     let nuevoEl = document.createElement('h2');
     nuevoEl.innerHTML = 'Hola!';
-    nuevoEl.setAttribute('class', 'extra');
+    nuevoEl.setAttribute('class', 'extraH2');
     extra.append(nuevoEl);
 });
